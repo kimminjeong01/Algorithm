554014e9fdf0ab211d2993a081730ab35e62ee88
minjeongkk <kunvx@naver.com>
2022-09-19 16:34:11 +0900

    괄호2

--- Baekjoon/자료구조/9012.py
+++ Baekjoon/자료구조/9012.py
@@ -2,23 +2,42 @@
 # 괄호    
 
 num = int(input())
- 
 for i in range(num):
-    stack = []
-    str = input()
-
+    str= input()
+    sum = 0
     for char in str:
-        if char=='(':
-            stack.append(char)
-        elif len(stack)!=0:
-            if char==')' and stack[len(stack)-1]=='(':
-                stack.pop()
-        elif char==')' and len(stack)==0:
-            stack.append(char)
+        if char =="(":
+            sum +=1
+        elif char ==")":
+            sum -=1
         
-    if len(stack)==0:
+        if sum<0:
+            print("NO")
+            break
+        
+    if sum == 0:
         print("YES")
-    else :
+    elif sum >0:
         print("NO")
+
+# num = int(input())
+ 
+# for i in range(num):
+#     stack = []
+#     str = input()
+
+#     for char in str:
+#         if char=='(':
+#             stack.append(char)
+#         elif len(stack)!=0:
+#             if char==')' and stack[len(stack)-1]=='(':
+#                 stack.pop()
+#         elif char==')' and len(stack)==0:
+#             stack.append(char)
+        
+#     if len(stack)==0:
+#         print("YES")
+#     else :
+#         print("NO")
 
     
