0a678b50f42599e15f791e44ba317df2173069e4
minjeongkk <kunvx@naver.com>
2022-09-27 14:03:15 +0900

    문자열, 집합

---
+++ Baekjoon/자료구조2/14425.py
@@ -0,0 +1,28 @@
+# 2022/ 09/ 27
+# 문자열 집합
+
+n, m = map(int, input().split())
+
+str_list=set()
+for i in range(n):
+    str_list.add(input())
+
+cnt = 0
+for i in range(m):
+    str = input()
+    if str in str_list:
+        cnt+=1
+
+print(cnt)
+
+import sys
+input = sys.stdin.readline
+
+# N, M = map(int, input().split())
+# s = set([input() for _ in range(N)])
+# cnt = 0
+# for _ in range(M):
+#     t = input()
+#     if t in s:
+#         cnt += 1
+# print(cnt)
