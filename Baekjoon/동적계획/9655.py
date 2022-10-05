2aea711d2e75bd4d84fe7b6b4acd4e242fdfe487
minjeongkk <kunvx@naver.com>
2022-10-05 16:41:43 +0900

    동적계획

---
+++ Baekjoon/동적계획/9655.py
@@ -0,0 +1,27 @@
+# 2022/10/05
+# 돌게임
+
+N = int(input())
+result = [-1]*(N+1)
+
+result[1]=1 # sk
+result[2]=0 # cy
+result[3]=1 # sk
+
+for i in range(4,N+1):
+    if result[i-1]==1 or result[i-3]==1:
+        result[i]=0
+    else:
+        result[i]=1
+
+if result[N]==1:
+    print("SK")
+else:
+    print("CY")        
+
+# if N%2==0:
+#     print('CY')
+# else:
+#     print('SK')
+
+
