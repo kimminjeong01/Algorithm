4e3494dc4dc6021b75e6432498a508b1a9102a18
minjeongkk <kunvx@naver.com>
2023-05-04 17:59:34 +0900

    최소 힙

---
+++ Baekjoon/자료구조/1927.py
@@ -0,0 +1,18 @@
+# 2023/05/04
+# 최소 힙
+
+import heapq
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+heap=[]
+for i in range(N):
+    x = int(sys.stdin.readline().rstrip())
+    if x==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, x)
+        
